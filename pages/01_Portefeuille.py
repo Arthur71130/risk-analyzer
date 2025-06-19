40249caba5deb755a2251fb6f1d964a541167660
Arthur71130 <159421235+Arthur71130@users.noreply.github.com>
2025-06-19 16:43:49 +0200

    Update 01_Portefeuille.py

--- pages/01_Portefeuille.py
+++ pages/01_Portefeuille.py
@@ -10,3 +10,9 @@ import streamlit as st
 
 st.title("Dashboard Portefeuille")
 st.write("Visualisation des investissements")
+
+
+# Option de déconnexion
+if st.button("Se déconnecter"):
+    st.session_state.logged_in = False
+    st.switch_page("app.py")
