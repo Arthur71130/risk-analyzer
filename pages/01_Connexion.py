e1c0f8f08227e2ae832fb511dc24d0d8779e235c
Arthur71130 <159421235+Arthur71130@users.noreply.github.com>
2025-06-19 16:14:50 +0200

    Add files via upload

---
+++ pages/01_Connexion.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 19 16:11:27 2025
+
+@author: arthurdemacedo
+"""
+
+import streamlit as st
+
+st.title("Page de Connexion / Création de compte")
+st.write("Créee un compte")
+
---
+++ pages/02_Portefeuille.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 19 16:12:47 2025
+
+@author: arthurdemacedo
+"""
+
+import streamlit as st
+
+st.title("Dashboard Portefeuille")
+st.write("Visualisation des investissements")
